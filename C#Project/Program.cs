21322a6d39c7e689175087b572e1a07d9398a702
Paulina <polina.bordian@gmail.com>
2023-03-29 17:22:02 +0300

    C# project1

--- C#Project/Program.cs
+++ C#Project/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿string[] array1 = new string[5] {"1234", "18", "hello", "96", "03"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
